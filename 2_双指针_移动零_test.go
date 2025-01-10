25ffce47c5f701d3411b12df6f94534ce7a6959e
chensen <1757606126@qq.com>
2025-01-10 21:22:51 +0800

    题目

--- 2_双指针_移动零_test.go
+++ 2_双指针_移动零_test.go
@@ -6,30 +6,19 @@ import (
 )
 
 func moveZeroes(nums []int) {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := 0; j < len(nums)-i-1; j++ {
-			if nums[i] == 0 {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
-			}
-		}
-	}
-}
-
-func sort(num []int) {
-	for i := 0; i < len(num)-1; i++ {
-		if num[i] == 0 {
-			for j := 0; j < len(num)-i-1; j++ {
-				if num[j] < num[j+1] {
-					num[j], num[j+1] = num[j+1], num[j]
-				}
-			}
+	n := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			t := nums[i]
+			nums[i] = 0
+			nums[n] = t
+			n++
 		}
 	}
 }
 
 func TestMoveZeroes(t *testing.T) {
 	nums := []int{0, 1, 0, 3, 12}
-	//moveZeroes(nums)
-	sort(nums)
+	moveZeroes(nums)
 	fmt.Println(nums)
 }
